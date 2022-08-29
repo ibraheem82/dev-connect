30afc4b58597108f4affdfdd8409541f99473a14
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-29 19:33:57 +0100

    message Changed to message.

--- basic/users/models.py
+++ basic/users/models.py
@@ -43,7 +43,7 @@ class Skill(models.Model):
         return str(self.name)
     
     
-class message(models.Model):
+class Message(models.Model):
     sender      = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
     recipient   = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name="messages")
     name        = models.CharField(max_length=200, null=True, blank=True)
