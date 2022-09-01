a449503d7562a103d6e7db8d540c931f8f265d59
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-09-01 15:01:56 +0100

    pk -> was added for the message id.

--- basic/users/urls.py
+++ basic/users/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path('delete-skill/<str:pk>/', views.deleteSkill, name="delete-skill"),
     
     path('inbox/', views.inbox, name="inbox"),
-    path('message/', views.viewMessage, name="message"),
+    path('message/<str:pk>/', views.viewMessage, name="message"),
 ]
