b7009e7f1cd8866c548734ca29005945c7bf212f
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-09-04 16:37:22 +0100

    Recipient view logic created.

--- basic/users/views.py
+++ basic/users/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.db.models import Q
 from .models import Profile, Skill, Message
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 
 from .utils import searchProfiles, paginateProfiles
 
@@ -248,4 +248,33 @@ def viewMessage(request, pk):
         message.save()
     context = {'message' : message}
     return render(request, 'users/message.html', context)
-    
+
+
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id = pk)
+    form  = MessageForm()
+    try:
+        sender  = request.user.profile
+        
+    except: 
+        sender = None
+        
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit = False)
+            message.sender = sender
+            message.recipient = recipient
+            
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+            messages.success(request, 'Your messages was successfully sent!')
+            # * You will be redirected back to the reciever account.
+            return redirect('user-profile', pk = recipient.id)
+    context = {'recipient' : recipient, 'form' : form}
+    return render(request, 'users/message_form.html', context)
