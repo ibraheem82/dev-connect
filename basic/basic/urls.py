76e25ed4c11f4e8d7dc583f0c0fca7c1c18e6e4d
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-09-11 07:53:53 +0100

    template_name was added to each [urls].

--- basic/basic/urls.py
+++ basic/basic/urls.py
@@ -66,7 +66,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = "reset.html"), name = 'password_reset_confirm'),
 
 
-    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name = "reset_password_complete.html"), name = 'password_reset_complete'),
    
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
