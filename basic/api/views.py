72e598aa167944c06c21551567ee7be9ad4cf146
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-09-11 10:06:02 +0100

    created a function that handles the api routes  turning it into a jsonesponse.

---
+++ basic/api/views.py
@@ -0,0 +1,27 @@
+# * Wiil turn all our pytho data to [json].
+from django.http import JsonResponse
+
+# * by default the jsonResponse can only return a dictionary.
+def getRoutes(request):
+    routes = [
+        # * will return back a list of project objects
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        # * Anytime a user wants to vote on a specific project, they will be send to this route or anytime they click on the route they will be send here
+        {'POST': '/api/projects/id/vote'},
+
+        # ** This will generate a token for the use 
+        # * we are going to be able to generate a token for a user.
+        # * this is going to allow us login a user.
+        
+        # * Class Based views is going t help us achieve this 
+        {'POST': '/api/users/token'},
+        # ** this will be for a refresh token
+        # json webtoken do have an expiration date, so when a user's token expires which is going to be around five minutes, it makes should the user can still stay log     in
+        {'POST': '/api/users/token/refresh'}
+
+        ,
+        {'POST': '/api/projects/id/vote'},
+    ]
+    # [safe = ] tells us that we can return something that is more than a python dictionary
+    return JsonResponse(routes, safe=False)
